eed53e6f0e283a25c7852cd3152e2d8a2fce6703
Mike Street <mikestreety@gmail.com>
2017-04-26 17:16:37 +0100

    Remove png fallback

--- gulpfile.js
+++ gulpfile.js
@@ -25,15 +25,14 @@ var gulp = require('gulp');
 var $ = {
 	gutil: require('gulp-util'),
 	svgSprite: require('gulp-svg-sprite'),
-	svg2png: require('gulp-svg2png'),
 	size: require('gulp-size'),
-}
+};
 
 var changeEvent = function(evt) {
 	$.gutil.log('File', $.gutil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + basePaths.src + ')/'), '')), 'was', $.gutil.colors.magenta(evt.type));
 };
 
-gulp.task('svgSprite', function () {
+gulp.task('sprite', function () {
 	return gulp.src(paths.sprite.src)
 		.pipe($.svgSprite({
 			shape: {
@@ -61,17 +60,6 @@ gulp.task('svgSprite', function () {
 		}))
 		.pipe(gulp.dest(basePaths.dest));
 });
-
-gulp.task('pngSprite', ['svgSprite'], function() {
-	return gulp.src(basePaths.dest + paths.sprite.svg)
-		.pipe($.svg2png())
-		.pipe($.size({
-			showFiles: true
-		}))
-		.pipe(gulp.dest(paths.images.dest));
-});
-
-gulp.task('sprite', ['pngSprite']);
 
 gulp.task('watch', function(){
 	gulp.watch(paths.sprite.src, ['sprite']).on('change', function(evt) {
